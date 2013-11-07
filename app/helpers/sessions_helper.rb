924a23c0e6fccc615b0e09485c14656683496142
Nguyen Duc Giang <nguyen.duc.giang@framgia.com>
2013-11-07 14:09:17 +0700

    Complete Finish subject button

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,7 +26,7 @@ module SessionsHelper
 
   def signed_in_user
     unless signed_in?
-      store_location
+      store_location!
       redirect_to signin_url, notice: "Please sign in."
     end
   end
--- app/models/enrollment_subject.rb
+++ app/models/enrollment_subject.rb
@@ -1,4 +1,8 @@
 class EnrollmentSubject < ActiveRecord::Base
 	has_many :enrollment_tasks
 	belongs_to :enrollment
+
+	def self.finish_subject subject
+		subject.update_attributes status: "done"
+	end
 end
