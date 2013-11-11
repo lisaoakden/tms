0b100fe3840d20ae8dfdc1b517d12339d3e156a4
kynm <nguyen.manh.ky@framgia.com>
2013-11-11 10:08:29 +0700

    create enrollment controller

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,6 +1,6 @@
 class EnrollmentsController < ApplicationController
-	before_action :signed_in_user, only: [:update]
-	before_action :correct_user,   only: [:update]
+  before_action :signed_in_user, only: [:update, :show]
+  before_action :correct_user,   only: [:update]
 
 	def update
 		user = User.find params[:user_id], include: :enrollments
@@ -13,6 +13,13 @@ class EnrollmentsController < ApplicationController
     	end
     end
 	end
+  def show
+    @enrollment = current_user.enrollments
+      .find_by course_id: current_user.current_course_id
+    @enrollment_subjects = @enrollment.enrollment_subjects 
+    @activities = current_user.activities
+      .activities_course current_user.current_course_id, Activity::TEMP_TYPE
+  end
 
 	private
 	def correct_user
@@ -20,3 +27,4 @@ class EnrollmentsController < ApplicationController
     redirect_to root_url unless current_user? user
   end 
 end
+
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-  	if signed_in?
-  	  @enrollment = current_user.enrollments.find_by(
-  	    course_id: current_user.current_course_id)
-  	  @enrollment_subjects = @enrollment.enrollment_subjects 
-      @activities = current_user.activities.activities_course(
-      	current_user.current_course_id, 2)
-  	end
+    if signed_in?
+      enrollment = current_user.enrollments
+       .find_by course_id: current_user.current_course_id
+      redirect_to user_enrollment_path current_user, enrollment
+    end
   end
 end
