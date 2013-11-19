1e60062d8f84f89ddaeca4630d09a37dcef4347a
Truong My Hai Ninh <truong.my.hai.ninh@framgia.com>
2013-11-19 14:39:47 +0700

    Update GUI for course index

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CoursesController < ApplicationController
+  layout "admin"
   before_action :choose_course, only: [:show, :index]
   before_action :correct_supervisor, only: [:update]
 
