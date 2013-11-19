d7938364bdc680a5079b1c81ff1257685476351a
duonglan <duong.thi.lan@framgia.com>
2013-11-19 11:40:52 +0700

    Start course

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -1,4 +1,18 @@
 class Admin::CoursesController < ApplicationController
+  before_action :correct_supervisor, only: [:update]
+
+  def update
+    if supervisor_signed_in?
+      @course = current_supervisor.courses.find params[:id]
+      unless @course.activated?
+        @course.update_attributes status: "start"
+      end
+      redirect_to admin_supervisor_course_path(current_supervisor.id, @course.id)
+    else
+      redirect_to root_path
+    end
+  end
+
   def show
   	if supervisor_signed_in?
       @supervisor = Supervisor.find params[:supervisor_id]
@@ -17,4 +31,10 @@ class Admin::CoursesController < ApplicationController
       redirect_to root_url
     end
   end
+
+  private
+  def correct_supervisor
+    supervisor = Supervisor.find params[:supervisor_id]
+    redirect_to root_url unless current_supervisor? supervisor
+  end 
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ActiveRecord::Base
-	has_many :enrollments
+	ACTIVATED = "start"
+  has_many :enrollments
 	has_many :activities,  foreign_key: "course_id", class_name: Activity.name
 	has_many :users, through: :enrollments
 	has_many :course_subjects
@@ -8,6 +9,10 @@ class Course < ActiveRecord::Base
 	has_many :supervisors, through: :supervisor_courses
 
   scope :choose_course, ->course_id {find course_id}
+
+  def activated?
+    self.status == ACTIVATED
+  end
 
   def course_duration
   	((self.end_date - self.start_date) / 1.day).to_i
