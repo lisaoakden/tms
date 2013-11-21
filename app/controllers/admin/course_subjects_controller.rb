6e75a5a14122bacdeb63cba2c328a25cbc98d0a7
kynm <nguyen.manh.ky@framgia.com>
2013-11-21 16:51:46 +0700

    edit find subject

--- app/controllers/admin/course_subjects_controller.rb
+++ app/controllers/admin/course_subjects_controller.rb
@@ -5,16 +5,16 @@ class Admin::CourseSubjectsController < ApplicationController
     @course_subject = CourseSubject.find params[:id]
     users = Enrollment.enrollment_subject_not_finish params[:course_id], params[:id]
     @course = current_supervisor.courses.find params[:course_id]
-    if users.present?
-      string_all_user_course_subject = users.collect(&:name).*(", ")
-      flash[:success] = string_all_user_course_subject
-      unless @course_subject.done?
-        @course_subject.update_attributes! status: CourseSubject::FINISH
+    if @course_subject.not_done?
+      @course_subject.update_attributes! status: CourseSubject::FINISH
+      if users.present?
         users.each do |user|
           enrollment_subject = user.current_enrollment.enrollment_subjects
-            .current_enrollment_subject(@course_subject.subject_id).first
+            .find_by subject_id: @course_subject.subject_id
           enrollment_subject.update_attributes! status: CourseSubject::FINISH
         end
+        string_all_user_course_subject = users.collect(&:name).*(", ")
+        flash[:success] = string_all_user_course_subject
       end
     end
     redirect_to admin_supervisor_course_path(current_supervisor.id, @course.id)
