bc2c3f0d545258f0aee20073bc679d46b41bc1ab
duonglan <duong.thi.lan@framgia.com>
2013-11-28 12:28:00 +0700

    edit_start_button

--- app/models/enrollment_subject.rb
+++ app/models/enrollment_subject.rb
@@ -1,26 +1,26 @@
 class EnrollmentSubject < ActiveRecord::Base
-	belongs_to :enrollment
-	belongs_to :subject
-	has_many :enrollment_tasks
+  belongs_to :enrollment
+  belongs_to :subject
+  has_many :enrollment_tasks
   has_many :activities, foreign_key: "subject_id", class_name: Activity.name
   accepts_nested_attributes_for :enrollment_tasks
   scope :finished_subject, ->{where status: Settings.status.finished}
   scope :current_enrollment_subject, ->subject_id{where subject_id: subject_id}
   scope :course_subject_not_finish, ->course_id, subject_id do
-    where course_id: course_id, subject_id: subject_id, status: Setting.status.new
+    where course_id: course_id, subject_id: subject_id, status: Settings.status.new
   end
 
-	def finish_subject!
-		self.update_attributes! status: Settings.status.finished
-	end
+  def finish_subject!
+    self.update_attributes! status: Settings.status.finished
+  end
 
-	def done?
-		self.status == Settings.status.finished
-	end
+  def done?
+    self.status == Settings.status.finished
+  end
 
-	def end_date
-		self.start_date + self.subject.duration.day
-	end
+  def end_date
+    self.start_date + self.subject.duration.day
+  end
   
   class << self
     def users_not_finish_subject course_id, subject_id
